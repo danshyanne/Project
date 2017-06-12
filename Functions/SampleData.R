89ef0aab05138cc5b6f991e19ba7a25a76620b20
Dansh3 <danshyanne@users.noreply.github.com>
2017-06-12 18:32:29 +1000

    Create SampleData.R

---
+++ Functions/SampleData.R
@@ -0,0 +1,5 @@
+sampleData <- function(tmpData, percent = 0.05) {  
+  numLines <- length(tmpData)  
+  tmpData <- sample(tmpData, size = numLines * percent, replace = FALSE)  
+  return(tmpData)
+}
